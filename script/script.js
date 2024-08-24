ddbefe2daace51fc2c462ddb9ee534c70f9c1434
alan4352 <94568593+alan4352@users.noreply.github.com>
2024-08-24 19:44:41 -0300

    finish

--- script/script.js
+++ script/script.js
@@ -1,12 +1,3 @@
 document.getElementById('cta-button').addEventListener('click', function() {
     alert('¡Gracias por tu interés! Pronto tendrás más información.');
 });
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
